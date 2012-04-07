c6288b3e8ae3eb351266c7dd185023510ae9adac
Anthony Letourneur <heybob47@hotmail.fr>
2012-04-07 09:27:55 +0200

    Ajout d'un champs css + Ajout de membre dans le bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import friendsofmine.Membre
+
 class BootStrap {
 
     def init = { servletContext ->
+		new Membre(nom:"Letourneur", prenom:"Anthony", mail:"let@mail.fr", mdp:"password", naissance:new Date("30/06/1988")).save(failOnError)
+		new Membre(nom:"Chapeyroux", prenom:"Sébastien", mail:"chap@mail.fr", mdp:"password", naissance:new Date("10/10/1987")).save(failOnError)
     }
     def destroy = {
     }
