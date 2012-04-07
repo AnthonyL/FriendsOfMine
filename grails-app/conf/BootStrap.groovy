58aae5b6321ef99ae85d8ffb269777f48fab3c3a
Anthony Letourneur <heybob47@hotmail.fr>
2012-04-07 09:31:13 +0200

    Modification du bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,8 +3,8 @@ import friendsofmine.Membre
 class BootStrap {
 
     def init = { servletContext ->
-		new Membre(nom:"Letourneur", prenom:"Anthony", mail:"let@mail.fr", mdp:"password", naissance:new Date("30/06/1988")).save(failOnError)
-		new Membre(nom:"Chapeyroux", prenom:"Sébastien", mail:"chap@mail.fr", mdp:"password", naissance:new Date("10/10/1987")).save(failOnError)
+		new Membre(nom:"Letourneur", prenom:"Anthony", mail:"let@mail.fr", mdp:"password", naissance:new Date("1988/06/30")).save(failOnError:true)
+		new Membre(nom:"Chapeyroux", prenom:"Sébastien", mail:"chaps@mail.fr", mdp:"password", naissance:new Date("1987/10/27")).save(failOnError:true)
     }
     def destroy = {
     }
